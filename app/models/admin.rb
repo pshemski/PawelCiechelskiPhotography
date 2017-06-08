cabb8ff018eed9d2b2c0de92889e6d4c2c50e390
Przemek Markowski <p.markowski1980@gmail.com>
2017-06-08 13:46:27 +0200

    removed rememberable from admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,8 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :rememberable, and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :trackable, :validatable
   mount_uploader :profile_picture, ImageUploader
   has_many :images       
 end
