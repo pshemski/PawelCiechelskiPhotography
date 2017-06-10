7309fbd0a4bffe07ccabdeaa6eb69161b683e069
Przemek Markowski <p.markowski1980@gmail.com>
2017-06-10 15:42:51 +0200

    configure email via sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,15 +65,15 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'pawelciechelskiphotography.com' }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default charset: "utf-8"
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "smtp.sendgrid.net",
     :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
