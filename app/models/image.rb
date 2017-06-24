b65bee6b205703e382dba037e703d6465c2b6ae3
Przemek Markowski <p.markowski1980@gmail.com>
2017-06-24 18:13:43 +0200

    fixed update image attributes

--- app/models/image.rb
+++ app/models/image.rb
@@ -8,10 +8,10 @@ class Image < ActiveRecord::Base
 	private
 
 	def update_image_attributes
-		if image.present? || image.image_changed?
+		if self.new_record? || self.image_changed?
 			self.image_file_size = image.file.size
 			self.image_content_type = image.file.content_type
-			self.landscape = true if image.file.is_landscape?(self.image)
+			self.landscape = true if image.is_landscape?(self.image)
 		end
 	end
 end
