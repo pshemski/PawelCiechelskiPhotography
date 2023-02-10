22183d9d1b1ea0a5dc3299011b1b423a8671383f
pshemski <p.markowski1980@gmail.com>
2023-02-10 16:34:34 +0000

    refactor specs

--- spec/features/devise/sessions_spec.rb
+++ spec/features/devise/sessions_spec.rb
@@ -1,46 +1,40 @@
 require 'rails_helper'
+require_relative '../../support/new_session_form'
 
 feature 'sessions' do
   let(:admin) { Admin.create(email: 'user@example.com', password: 'caplin') }
+  let(:new_session_form) { NewSessionFrom.new }
 
   scenario 'new session page' do
-    visit '/admin'
+    new_session_form.visit_new_session_page
 
-    expect(page).to have_content('password')
+    expect(page).to have_content('Log in')
   end
 
   scenario 'create session with invalid credentials' do
-    visit '/admin'
-    within("#new_admin") do
-      fill_in 'Email', with: 'user@example.com'
-      fill_in 'Password', with: 'wrongpassword'
-    end
-
-    click_on('Log in')
+    new_session_form.visit_new_session_page.fill_the_form_with(
+      email: admin.email,
+      password: 'wrongpassword'
+    ).submit
 
     expect(page).to have_content('Invalid Email or password.')
   end
 
   scenario 'create session with valid credentials' do
-    visit '/admin'
-    within("#new_admin") do
-      fill_in 'Email', with: admin.email
-      fill_in 'Password', with: 'caplin'
-    end
-
-    click_on('Log in')
+    new_session_form.visit_new_session_page.fill_the_form_with(
+      email: admin.email,
+      password: 'caplin'
+    ).submit
 
     expect(page).to have_content('Signed in successfully.')
   end
 
   scenario 'sign out' do
-    visit '/admin'
-    within("#new_admin") do
-      fill_in 'Email', with: admin.email
-      fill_in 'Password', with: 'caplin'
-    end
+    new_session_form.visit_new_session_page.fill_the_form_with(
+      email: admin.email,
+      password: 'caplin'
+    ).submit
 
-    click_on('Log in')
     click_on('Sign Out')
     expect(page).not_to have_content('Categories')
   end
