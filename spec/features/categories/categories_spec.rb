5a35e4a8421495f17acb9965e8d2ff570f8178b5
pshemski <p.markowski1980@gmail.com>
2023-02-14 14:58:33 +0000

    add specs for categories

---
+++ spec/features/categories/categories_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'categories' do
+  let(:admin) { Admin.create(email: 'user@example.com', password: 'caplin') }
+
+  before do
+    login_as admin
+  end
+
+  context 'when creating a new category' do
+    scenario 'with invalid attributes' do
+      visit '/'
+      click_on 'Categories'
+      click_on 'Add Category'
+      click_on 'Create Category'
+
+      expect(page).to have_content("Name can't be blank")
+    end
+
+    scenario 'with valid attributes' do
+      visit '/'
+      click_on 'Categories'
+      click_on 'Add Category'
+      fill_in 'Name', with: 'Test'
+      click_on 'Create Category'
+
+      expect(page).to have_content('New Category Created')
+    end
+  end
+end
