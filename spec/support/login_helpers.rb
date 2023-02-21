f59df2d6c9db0b3d2da7c40527e4a9bed17e33ff
pshemski <p.markowski1980@gmail.com>
2023-02-21 17:44:03 +0000

    include login helpers

---
+++ spec/support/login_helpers.rb
@@ -0,0 +1,11 @@
+module LoginHelpers
+  def login_as(admin)
+    if admin.nil?
+      allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, { scope: :admin })
+      allow(controller).to receive(:current_admin).and_return(nil)
+    else
+      allow(request.env['warden']).to receive(:authenticate!).and_return(admin)
+      allow(controller).to receive(:current_admin).and_return(admin)
+    end
+  end
+end
