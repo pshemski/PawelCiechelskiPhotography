9ebcd158ffb2f214267595134b49ad8e2db2b596
pshemski <p.markowski1980@gmail.com>
2023-02-23 15:44:55 +0000

    use letter opener in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,16 +23,17 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default charset: 'utf-8'
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: ENV['gmail_username'],
-    password: ENV['gmail_password'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.delivery_method = :letter_opener
+  # # SMTP settings for gmail
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   user_name: ENV['gmail_username'],
+  #   password: ENV['gmail_password'],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
