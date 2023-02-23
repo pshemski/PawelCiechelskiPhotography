7443aabb9b92fd3bc6afb511f63ba463b7e905d4
pshemski <p.markowski1980@gmail.com>
2023-02-23 15:44:30 +0000

    send an email after message saved

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
 class Message < ActiveRecord::Base
   validates_presence_of :name, :email, :message
   validates_format_of :email, with: Devise.email_regexp
+
+  after_save :send_email_to_admin
+
+  private
+
+  def send_email_to_admin
+    MessageMailer.new_message_notification(self).deliver_later
+  end
 end
